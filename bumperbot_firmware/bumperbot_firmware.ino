bc7ee1e950d0be7f0754c8716f5e8dd24d61e03f
Nick Segler <njsegler@alaska.edu>
2017-02-10 14:14:25 -0900

    added phone tilt controll
    It doesn’t work but the basics are there

--- bumperbot_firmware/bumperbot_firmware.ino
+++ bumperbot_firmware/bumperbot_firmware.ino
@@ -1,3 +1,5 @@
+//phone tilt controlls dont work
+
 //for wifi
 #include <ESP8266WiFi.h>
 #include <Wire.h>
@@ -23,11 +25,22 @@ const char webpage[] PROGMEM="<!DOCTYPE html>"
         //  drive with 3 parameters sends commands and keeps current speed command is a number as third parameter
         "function drive(L,R,C=0){if(C==0){var req='?'+L+','+R+'!';}else{var req ='?C,C!'+C+'&'}var xhr=new XMLHttpRequest();xhr.open('GET',req,!0);xhr.send();}"
 
-        //"function addKey(f){console.log(f.k.value+' '+f.a.value+'\n'+cons);}"
-
-        "function changeVal(id,val){document.getElementById(id).innerHTML=val;}"
-        "var lm=0;"
-        "var rm=0;"
+        //toggles tilt contoll and changes the button to display current state
+        "function toggleTilt(){"
+          "tilt=!tilt;"
+          "if(tilt){"     
+            "document.getElementById('ttog').innerHTML='on';"
+          "}else{"
+            "document.getElementById('ttog').innerHTML='off';"
+          "}"
+        "}"
+        
+        "var maxspeed=75;"  //used to scale tilt input
+        "var tilt=false;" //tilt toggle variable
+        "var center_gamma=-45;" //where the tilt controll is turned on
+        "var center_beta=0;"
+        
+        //a object full of keycodes associated with drivefunction calls
         "var cons={"//add more functions here, the keys should be the js keycode
           "' ':function() {drive(0,0)},"
           "stop:function() {drive(0,0)},"
@@ -38,6 +51,7 @@ const char webpage[] PROGMEM="<!DOCTYPE html>"
           "f:function() {drive(0,0,1)},"
           "g:function() {drive(0,0,2)}"
         "};"
+        //key board listener for keyboard controlled movement and functions
         "window.addEventListener('keydown', "
             "function (event) {"
               "if (event.defaultPrevented){return;}"
@@ -49,20 +63,39 @@ const char webpage[] PROGMEM="<!DOCTYPE html>"
               "}"
                 "event.preventDefault();},"
                 "true);"
-                
+        //adds a device orientation listener for WIP tilt controlls 
+        "window.addEventListener('deviceorientation',"
+            "function(event){"
+            "console.log('logit'+event.beta);"
+              "if(tilt){"
+                "drive(event.gamma-center_gamma+(event.beta),event.gamma-center_gamma-(event.beta));"
+                "console.log('throt: '+(event.gamma-center_gamma)+event.beta);"
+               "}"
+               //debug to see current tilt info
+               "document.getElementById('disABG').innerHTML='A:'+event.alpha+' B:'+event.beta+' G:'+event.gamma+' betaOff:'+(center_beta-event.beta)+'gammaOff:'+(center_gamma-event.gamma);"
+               
+            "},"
+         "true);"
+   
       "</script>"
     "</head>"
     "<body>"
+    //controll buttons
     "<div class='wm'><input id='bfw' type='button' value='w' onclick='cons.w();'/></div><br>"
-    "<input id='bl' type='button' value='a' onclick='cons.d();'/>"
+    "<input id='bl' type='button' value='a' onclick='cons.a();'/>"
     "<input id='bbw' type='button' value='s' onclick='cons.s();'/>"
-    "<input id='br' type='button' value='d' onclick='cons.a();'/><br>"
-    
+    "<input id='br' type='button' value='d' onclick='cons.d();'/><br>"
     "<input id='bstp' type='button' value='space' onclick='cons.stop();'/>"
+    //toggle tilt controll button
+    "<br>Tilt controll is not working<br>Tilt toggle: <button id='ttog' onclick='toggleTilt()'>off</button>"
+    //space to display alpha beta gamma rotation data
+    "<br><br><span id='disABG'></span>"
+
+    
 
     //"<form>Key code:<input type='text' name='k'><br>"
     //"Action:<input type='text' name='a'><br>"
-    //"<input type='button' value='add' onclick=''></form>"
+    //"<input type='button' value='add' onclick=''>tog</form>"
     "</body>"
   "</html>";
 
@@ -100,7 +133,7 @@ void setup() {
 
   digitalWrite(D3,HIGH);
 }
-
+//make ToggleP pin controllers for the builtin LED and pin D3
 ToggleP light(LED_BUILTIN);
 ToggleP light2(D3);
 
@@ -127,10 +160,14 @@ void loop() {
   client.flush();
 }
 
-//
+//processes the request sent by from the wweb page
 void doRequest(String r){
+  //pulls the motor controll info frim the request string
   String m=r.substring(r.indexOf("?")+1,r.indexOf("!"));
+
+  //checks if the motor controll should be ignored
   if(m.substring(0,m.indexOf(","))!="C"){
+    //sets motors to the speed in the request
     speed_l=m.substring(0,m.indexOf(",")).toInt();
     if(speed_l<0)
     {
@@ -145,6 +182,7 @@ void doRequest(String r){
       speed_r*=-1;
     }else if(turn_r!=_CW){turn_r=_CW;}
   }else{
+    //gets the command info and runs its function
     String c=r.substring(r.indexOf("!")+1,r.indexOf("&"));
     switch(c.toInt()){//add command functions here
       case(1): 
@@ -157,7 +195,5 @@ void doRequest(String r){
         break;
     }
   }
- 
-  Serial.print(m);
 }
 
